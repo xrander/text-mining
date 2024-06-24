64ce408fef50157f65dbee8daf9c181428b4007c
xrander <olamideadu@gmail.com>
2024-06-24 17:58:25 +0200

    Sentiment analysis using tidytext

--- tidy-text.r
+++ tidy-text.r
@@ -68,7 +68,7 @@ frequency <- bind_rows(
   mutate(tidy_bronte, author = "Brontë Sisters"),
   mutate(tidy_hgwells, author = "H.G. Wells"),
   mutate(tidy_books, author = "Jane Austen")
-) |>
+) |> 
   select(-c(4:6)) |> 
   mutate(
     word = str_extract(word, "[a-z]+")
@@ -78,13 +78,143 @@ frequency <- bind_rows(
     .by = author,
     proportion = n/sum(n)
   ) |> 
-  select(-n) |> 
-  arrange(word) |> 
-  relocate(word)
+  pivot_wider(
+    id_cols = word,
+    names_from = author,
+    values_from = proportion
+  ) |> 
+  pivot_longer(
+    cols = c(2, 3),
+    names_to = "author",
+    values_to = "proportion"
+  ) |> 
+  janitor::clean_names()
 
-ggplot()
+frequency |> 
+  ggplot(aes(proportion, jane_austen, color = abs(jane_austen- proportion))) +
+  geom_abline(
+    color = "gray40",
+    linetype = 2
+  ) +
+  geom_jitter(
+    alpha = .1,
+    size = 2.5, 
+    width = .3,
+    height = .3
+  ) +
+  geom_text(
+    aes(label = word),
+    check_overlap = TRUE,
+    width = .3,
+    height = .3,
+    vjust = 1.5
+  ) +
+  scale_x_log10(
+    labels = scales::percent_format()
+  ) +
+  scale_y_log10(
+    labels = scales::percent_format()
+  ) +
+  scale_color_gradient(
+    limits = c(0, .001),
+    low = "darkslategray4",
+    high = "gray75"
+  ) +
+  labs(
+    y = "Jane Austen",
+    x = NULL
+  ) +
+  facet_wrap(~author) +
+  theme_minimal() +
+  theme(
+    legend.position = "none"
+  )
+
+cor.test(data = frequency[frequency$author == "Brontë Sisters",],
+         ~ proportion + jane_austen)
+
+cor.test(data = frequency[frequency$author == "H.G. Wells",],
+         ~ proportion + jane_austen)
 
 
-alice_wl <- gutenberg_download(c(28885, 38065))
+# Sentiment Analysis -----------------------------------------------
+tidy_books |> 
+  filter(book == "Emma") |> 
+  inner_join(
+    get_sentiments("nrc") |> 
+      filter(sentiment == "joy"),
+    join_by(word)
+  ) |> 
+  count(word, sort = TRUE)
 
-alice_wl
+janeaustensentiment <- tidy_books |> 
+  inner_join(
+    get_sentiments("bing"),
+    join_by(word)
+  ) |> 
+  count(book, index = line_number %/% 80, sentiment) |> 
+  pivot_wider(
+    names_from = sentiment,
+    values_from = n,
+    values_fill = 0
+  ) |> 
+  mutate(
+    sentiment = positive - negative
+  )
+
+ggplot(
+  janeaustensentiment,
+  aes(index, sentiment, fill = book)
+) +
+  geom_col(
+    show.legend = FALSE
+  ) +
+  facet_wrap(~book, scales = "free")
+
+pride_prejudice <- tidy_books |> 
+  filter(book == "Pride & Prejudice")
+
+afinn <- pride_prejudice |> 
+  inner_join(get_sentiments("afinn")) |> 
+  group_by(index = line_number %/%  80) |> 
+  summarize(sentiment = sum(value)) |> 
+  mutate(
+    method = "AFINN"
+  )
+
+bing_nrc <- bind_rows(
+  pride_prejudice |> 
+    inner_join(get_sentiments("bing")) |> 
+    mutate(method = "Bing et al."),
+  pride_prejudice |> 
+    inner_join(get_sentiments("nrc")) |> 
+    filter(sentiment %in% c("positive", "negative")) |> 
+    mutate(method = "nrc")
+)
+
+bing_nrc <- bing_nrc |> 
+  count(method, index = line_number %/% 80, sentiment) |> 
+  pivot_wider(
+    names_from = sentiment,
+    values_from = n
+  ) |> 
+  mutate(sentiment = positive - negative)
+
+
+afinn |> 
+  bind_rows(
+    bing_nrc |> 
+      relocate(
+        index, sentiment, method
+      ) |> 
+      select(1:3)
+  ) |> 
+  ggplot(
+    aes(index, sentiment, fill = method)
+  ) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(
+    ~method,
+    scales = "free_y",
+    nrow = 3
+  )
